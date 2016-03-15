9ed2098f59f6844494f68d0d0f71a394dde97567
Otakar Valek <otakar@Otakars-MacBook.local>
2016-03-15 15:33:00 -0700

    byte[]->float[] conversion

--- PitchDetector.java
+++ PitchDetector.java
@@ -1,6 +1,15 @@
-package stuner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pitchdetector;
 
-import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -10,92 +19,132 @@ import javax.sound.sampled.TargetDataLine;
 
 public class PitchDetector {
 	
-	private static final float SAMPLE_RATE = 8000; 					//8000, 11025, 16000, 22050, 44100    - samples/sec
-	private static final int SAMPLE_SIZE_IN_BITS = 16;				// 8, 16
-	private static final int CHANNELS = 1;							// 1-mono, 2-stereo									
-	private static final boolean SIGNED = true;						// true = both positive/negative values in sample, false = positive only
-	private static final boolean BIG_ENDIAN = true;
-	
-	
-	AudioFormat audioFormat;
-	TargetDataLine line;
-	DataLine.Info info;
-	
+    private static float sampleRate;
+    private static int sampleSizeInBits;
+    private static int channels;
+    private boolean signed;
+    private boolean bigEndian;
+        
+    boolean stopped;
+    int numBytesRead;
+    float [] f_buffer;
 		
-	//-------------------------------------------------------------------------
-	public PitchDetector(){
-		audioFormat = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE_IN_BITS, CHANNELS, SIGNED, BIG_ENDIAN);				
-		info = new DataLine.Info(TargetDataLine.class, audioFormat);
+    //-------------------------------------------------------------------------	
+    public PitchDetector(){
 		
+        sampleRate = 8000.0f;                                                   //8000, 11025, 16000, 22050, 44100    - samples/sec
+        sampleSizeInBits = 16;                                                  // 8, 16
+        channels = 1;                                                           // 1 - mono, 2-stereo
+        signed = true;                                                          
+        bigEndian = false;
+        
+        stopped = false;	
+    }
+	
+	
+    //-------------------------------------------------------------------------
+    public void captureAudio(){
 
-	}
-	
-	//-------------------------------------------------------------------------
-	public void captureAudio(){
-		
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);                      
+        TargetDataLine line = null;
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format); // format is an AudioFormat object
+        
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.print("\nAudio Line not supported");
+            System.exit(-1);
+        }
 
-		if (!AudioSystem.isLineSupported(info)) {
-		    System.out.print("\nInput Error: This line is not supported"); 
-		}
-		
-		try {
-		    line = (TargetDataLine) AudioSystem.getLine(info);
-		    line.open(audioFormat);
-		} catch (LineUnavailableException ex) {
-			System.out.print("\nException occured\n");
-		    ex.printStackTrace(); 
-		}
-		
-		
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException ex) {
+            System.out.print("\nLine Unavailable: " + ex + "\n");
+            System.exit(-2);
+        }
+                     
+        byte[] buffer = new byte[line.getBufferSize() / 5];        
+        f_buffer = new float[buffer.length / 4];                                // 4 bytes = 1 float
 
-		ByteArrayOutputStream out  = new ByteArrayOutputStream();
-		int numBytesRead;
-		byte[] data = new byte[line.getBufferSize() / 5];
+        line.start();
 
+        while (!stopped) {
+    	 
+            numBytesRead =  line.read(buffer, 0, buffer.length);            
+            printBuffer(buffer);                                                // DEBUG LINE
+                        
+            f_buffer = convToFloat(buffer);
+            printFBuffer(f_buffer);                                             // DEBUG LINE
+                                               
+        }          
+    }
+		    
+    //-------------------------------------------------------------------------
+    // This method accepts byte array, converts its values to float (4 bytes = 1 float) and 
+    //             returns float array with converted values.
+    // @param:  source  - the byte array to be converted
+    // @return: fArr    - float array with converted values  
+    //-------------------------------------------------------------------------
+    public static float[] convToFloat(byte [] source){
 
-		line.start();
-
-
-		while (line.read(data, 0, data.length) > 0) {
-			
-		   // Read the next chunk of data from the TargetDataLine.
-		   numBytesRead =  line.read(data, 0, data.length);
-		   
-		   // Save this chunk of data.
-		   out.write(data, 0, numBytesRead);
-		   
-		   System.out.print("\ndata array: " + numBytesRead + "\n");
-		   for (int i = 0; i < data.length; i++){
-			   
-			   System.out.print(" " + data[0]);
-			   
-		   }
-		   
-		}   
+        ByteArrayInputStream bas = new ByteArrayInputStream(source);
+    
+        DataInputStream ds = new DataInputStream(bas);
+        float[] fArr = new float[source.length / 4];  // 4 bytes per float
+        
+        for (int i = 0; i < fArr.length; i++){
+        
+            try {
+                fArr[i] = ds.readFloat();
+            } catch (IOException ex) {
+                Logger.getLogger(PitchDetector.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.print("\nI/O Exception: " + ex);
+                System.exit(-2);
+            }
+        }
+  
+        return fArr;  
+    }
+    
+    
+    // DEBUG HELPERS
+    //-------------------------------------------------------------------------
+    public void printBuffer(byte[] bfr){
+		
+        System.out.print("\n\nByte Buffer size: " + numBytesRead + "\n");
+        
+        for(int i = 0; i < bfr.length; i++){        	
+        	System.out.print(" " + bfr[i]);
+        }			
+    }
+    
+    
+    
+    public void printFBuffer(float[] bfr){
+		
+        System.out.print("\n\nFloat Buffer size:" + bfr.length + "\n");
+        
+        for(int i = 0; i < bfr.length; i++){        	
+        	System.out.print(" " + bfr[i]);
+        }			
+    }
+    
+    
+	
+	
+	
+	
+	
+    
+    // ENABLED FOR UNIT TESTING	
+    //-------------------------------------------------------------------------
+    public static void main(String args[]){
+		
+	PitchDetector p1 = new PitchDetector();
+	p1.captureAudio();
 		
 		
-		
-		
-		
-	}
-	
-	
-	public static void main (String args[]){
-		
-		PitchDetector p1 = new PitchDetector();
-		p1.captureAudio();
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+    }
+    
+   
+    
 }

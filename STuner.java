1c28d3d9b3c9011009596012e32c8eda4e9a738f
ovalek <ovalek@sfu.ca>
2016-03-18 19:28:58 -0700

    WAV file input added

--- STuner.java
+++ STuner.java
@@ -1,8 +1,18 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
+
+
+
+
 
 public class STuner {
 
@@ -15,15 +25,57 @@ public class STuner {
     public static boolean signed = true;
     public static boolean bigEndian = false;
 
-    public static void main(String args[]) {
+    public static void main(String args[]) {    	
         TunerFrame frame = new TunerFrame();
-        frame.setVisible(true);
+        frame.setVisible(true);        
+        
         PitchDetector detector = new PitchDetector();
-        TargetDataLine microphone = getMicrophone();
+        TargetDataLine microphone = getMicrophone();        
 
+        // --------------------------------------------------------------------
+        // USING SOUND FILE
+        // --------------------------------------------------------------------
+    	int totalFramesRead = 0;
+        File fileIn = new File("146.83Hz.wav");
+        
+    	try {
+    		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileIn);
+      	  	int bytesPerFrame = audioInputStream.getFormat().getFrameSize();
+      	  	
+      	  	if (bytesPerFrame == AudioSystem.NOT_SPECIFIED) {
+      	  		bytesPerFrame = 1;
+      	  	}
+      	  
+      	  	// Set an arbitrary buffer size of 1024 frames.
+      	  	int numBytes = 1024 * bytesPerFrame; 
+      	  	byte[] audioBytes = new byte[numBytes];
+      	  
+      	  	try {
+      	  		int numBytesRead = 0;
+      	  		int numFramesRead = 0;
+      	  		// Try to read numBytes bytes from the file.
+      	  		while ((numBytesRead = audioInputStream.read(audioBytes)) != -1) {
+
+      	  			// Calculate the number of frames actually read.
+      	  			numFramesRead = numBytesRead / bytesPerFrame;
+      	  			totalFramesRead += numFramesRead;
+      	  			
+      	  			float pitch = detector.getPitch(audioBytes, sampleRate);      	  			
+      	  		}
+      	  	} catch (Exception ex) { 
+      	  		System.out.print("File Read error" + ex);
+      	  	}
+      	} catch (Exception e) {
+      		System.out.print("I/O error" + e);
+      	}
+        
+        // --------------------------------------------------------------------
+        // USING MICROPHONE
+        // --------------------------------------------------------------------
+    	/*
         byte[] audioBytes = new byte[bufferSize * bytesPerFrame];
         microphone.start();
-
+               
         while (true) {
             microphone.read(audioBytes, 0, audioBytes.length);
             float pitch = detector.getPitch(audioBytes, sampleRate);
@@ -31,6 +83,8 @@ public class STuner {
             // update GUI
             // System.out.println(pitch);
         }
+        */
+  
     }
 
     private static TargetDataLine getMicrophone() {
@@ -56,4 +110,5 @@ public class STuner {
 
         return mic;
     }
+   
 }
